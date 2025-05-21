26bdbfcca44e713be7d42ea61ad503ffaae7d132
TIGER <suraj23082002@gmail.com>
2025-05-21 17:22:07 +0530

    changed baseurl for deploy

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3000/api',
-    // baseURL: "https://student-management-system-42m1.onrender.com/api",
+    // baseURL: 'http://localhost:3000/api',
+    baseURL: "https://student-management-system-42m1.onrender.com/api",
 });
 
 export default axiosInstance;
