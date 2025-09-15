74d97d75470cee8bd88f15584886383b00a6f857
TIGER <suraj23082002@gmail.com>
2025-09-15 17:29:28 +0530

    fixed the axios base url

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // baseURL: 'http://localhost:3000/api',
-    baseURL: "https://student-management-system-xi-six.vercel.app/api",
+    baseURL: "https://student-management-system-42m1.onrender.com/api",
 });
 
 export default axiosInstance;
